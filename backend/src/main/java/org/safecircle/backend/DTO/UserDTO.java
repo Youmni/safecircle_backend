a11cfaef63b989fd280e345474fed3430860595b
Youmni <youmni.malha@gmail.com>
2024-12-11 16:20:55 +0100

    Added: UserService and necessary DTOs

---
+++ backend/src/main/java/org/safecircle/backend/DTO/UserDTO.java
@@ -0,0 +1,58 @@
+package org.safecircle.backend.DTO;
+
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+
+    public UserDTO(String firstName, String lastName, String email, String password, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- backend/src/main/java/org/safecircle/backend/services/CircleService.java
+++ backend/src/main/java/org/safecircle/backend/services/CircleService.java
@@ -1,7 +1,12 @@
 package org.safecircle.backend.services;
 
+import org.safecircle.backend.repositories.CircleRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CircleService {
+
+    private CircleRepository circleRepository;
+
+
 }
