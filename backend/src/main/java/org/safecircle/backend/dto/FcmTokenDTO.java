02249d9d8666922589a03a131d9b292c3afd4754
Youmni <youmni.malha@gmail.com>
2024-12-19 10:09:26 +0100

    Update fcm token dto

--- backend/src/main/java/org/safecircle/backend/dto/FcmTokenDTO.java
+++ backend/src/main/java/org/safecircle/backend/dto/FcmTokenDTO.java
@@ -3,7 +3,6 @@ package org.safecircle.backend.dto;
 public class FcmTokenDTO {
     private String fcmToken;
 
-    private long userId;
 
     public String getFcmToken() {
         return fcmToken;
@@ -11,13 +10,5 @@ public class FcmTokenDTO {
 
     public void setFcmToken(String fcmToken) {
         this.fcmToken = fcmToken;
-    }
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
     }
 }
